6e56a5104639c4d47ad68add12d54684ac1308f0
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-27 13:08:37 +0200

    URL Query Parameters

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,8 +1,16 @@
+import { NextRequest } from "next/server"; // Object | Provide methods to managing query parameters easily.
 import { comments } from "./data";
 
+
+
 // GET all comments
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams; // Access all query parameters
+  const query = searchParams.get('query'); // get parameters called query
+  const filteredComments = query ? comments.filter(comment => comment.text.includes(query)) : comments;
+  
+
+  return Response.json(filteredComments);
 }
 
 // POST (Add) a comment
